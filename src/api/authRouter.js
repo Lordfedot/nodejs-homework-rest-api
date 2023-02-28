b7492bf4096f668b57f0d3895a99195b20358082
Lordfedot <orestmazgal@gmail.com>
2023-02-28 15:19:25 +0200

    1

--- src/api/authRouter.js
+++ src/api/authRouter.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const { asyncWrapper } = require("../helpers/apiHelpers");
-const { getAuthValidation } = require("../middlewares/middlewaresValidation");
+const {
+  getAuthValidation,
+  getSubscriptionValidation,
+} = require("../middlewares/middlewaresValidation");
 const { authMiddleware } = require("../middlewares/authMiddleware");
 const {
   registrationCtrl,
   loginCtrl,
   logoutCtrl,
   currentCtrl,
+  updateSubscriptionCtrl,
 } = require("../controllers/authControllers");
 
 const router = express.Router();
@@ -15,5 +19,11 @@ router.post("/register", getAuthValidation, asyncWrapper(registrationCtrl));
 router.post("/login", getAuthValidation, asyncWrapper(loginCtrl));
 router.post("/logout", authMiddleware, asyncWrapper(logoutCtrl));
 router.get("/current", authMiddleware, asyncWrapper(currentCtrl));
+
+router.patch(
+  "/",
+  [authMiddleware, getSubscriptionValidation],
+  asyncWrapper(updateSubscriptionCtrl)
+);
 
 module.exports = { authRouter: router };
